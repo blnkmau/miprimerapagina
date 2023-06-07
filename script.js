264eee8842ebe18ba14455652278b0c2d16c7dc4
blnkmau <toulouse86@gmail.com>
2023-06-07 14:24:55 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function showAlert() { alert('Tomá por curiosx 8======D'); }
+     document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+    document.querySelector(".nav-links").
+    classList.toggle("nav-links-responsive")})
+
+const form = document.getElementById('myForm');
+    form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    validateForm(); });
+                      
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+        }
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+    alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+    alert('Correo electrónico enviado correctamente.');
+    } }
